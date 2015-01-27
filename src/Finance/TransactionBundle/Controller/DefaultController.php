2e8230d8c18183d88033c4ad984f0fd57dd2db9c
Javier Garcia <javier.tb@outlook.com>
2015-01-27 12:44:41 +0100

    routing, fixtures, jsonResponse

--- src/Finance/TransactionBundle/Controller/DefaultController.php
+++ src/Finance/TransactionBundle/Controller/DefaultController.php
@@ -3,11 +3,32 @@
 namespace Finance\TransactionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('TransactionBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function userTransactionAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $transactions = $em->getRepository('TransactionBundle:Transaction')->findByUser($id);
+
+        return new JsonResponse($transactions);
+    }
+
+    public function allTransactionAction(){
+
+        $em = $this->getDoctrine()->getManager();
+
+        $transactions = $em->getRepository('TransactionBundle:Transaction')->findAll();
+
+        return new JsonResponse($transactions);
     }
 }
---
+++ src/Finance/TransactionBundle/DataFixtures/ORM/Transactions.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Finance\TransactionBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Finance\TransactionBundle\Entity\Transaction;
+
+class Transactions extends AbstractFixture implements OrderedFixtureInterface
+{
+
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 10;
+
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        for($i = 0; $i < 100; $i++){
+
+            $transaction = new Transaction();
+
+            $transaction->setAmount(mt_rand(0*10, 10000*10)/100);
+            $transaction->setTransactionDate(new \DateTime('now + '.rand(0, 7).' days'));
+            $transaction->setUser(mt_rand(1, 3));
+
+            $manager->persist($transaction);
+            $manager->flush();
+        }
+
+    }
+}
