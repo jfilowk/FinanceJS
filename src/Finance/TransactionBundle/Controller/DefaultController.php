1ce5ff03c0787f433a2027514df1257a6bf5ac6a
Javier Garcia <javier.tb@outlook.com>
2015-01-27 11:22:33 +0100

    TransactionBundle created

---
+++ src/Finance/TransactionBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Finance\TransactionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TransactionBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
